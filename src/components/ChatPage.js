d0935551917d076608595687cc8877d3ec8ecf05
Sai1Ram <sairamsenapati0022@gmail.com>
2023-06-11 16:00:44 +0530

    chat page

--- src/components/ChatPage.js
+++ src/components/ChatPage.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import maleUser from '../assets/images/male-user.jpg'
 import femaleUser from '../assets/images/female-user.jpg'
+import { useNavigate } from 'react-router-dom'
 const ChatPage = () => {
+   const navigate = useNavigate();
+   useEffect(()=>{
+      if(!localStorage.getItem("userInfo")){
+         navigate("/signIn");
+      }
+   },[navigate])
+   const [searchToggle, setSearchToggle] = useState(false);
+   const [searchText, setSearchText] = useState("");
+   const handleSearchToggle = ()=>{
+      setSearchToggle(true);
+   }
+   const handleSearch = async() =>{
+      console.log("sairam");
+      const userData = localStorage.getItem("userInfo");
+      const token = JSON.parse(userData).token;
+      const response = await fetch(`http://localhost:5000/auth/user/?search=${searchText}`, {method: "GET", headers:{"Content-Type": "application/json", "Authorization":`Bearer ${token}`}});
+      console.log(response);
+   }
+   const handleSearchText = (e) =>{
+      setSearchText(e.target.value);
+   }
   return (
    <>
    
@@ -24,18 +46,26 @@ const ChatPage = () => {
             <span className="text-lg text-gray-600">Junior Developer</span>
          </div>
       </div>
-      <div className="flex items-center space-x-2">
-         <button type="button" className="inline-flex items-center justify-center rounded-lg border h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
+      <div className="flex items-center gap-2">
+         {searchToggle && (<div className={`w-full gap-2 h-5 flex items-center justify-end`}>
+            <input type="text" className={`outline-none border-b-2 border-x-0 border-t-0 border-gray-500`} onChange={handleSearchText} value={searchText}/>
+            <button type="button" onClick={handleSearch} className="inline-flex items-center justify-center rounded-lg  h-10 w-10 transition duration-500 ease-in-out text-gray-500 ">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
             </svg>
          </button>
-         <button type="button" className="inline-flex items-center justify-center rounded-lg border h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
+         </div>)}
+         {!searchToggle && <button type="button" onClick={handleSearchToggle} className="w-full flex items-center justify-center rounded-lg  h-10 text-gray-500">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
+               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+            </svg>
+         </button>}
+         <button type="button" className=" w-full flex items-center justify-center rounded-lg  h-10 text-gray-500">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
             </svg>
          </button>
-         <button type="button" className="inline-flex items-center justify-center rounded-lg border h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
+         <button type="button" className="w-full flex items-center justify-center rounded-lg h-10  text-gray-500">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>
             </svg>
